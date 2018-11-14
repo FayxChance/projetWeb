c32fc50fbd4f3b5a128672453761176429755156
Rémi <remibouvier99@gmail.com>
2018-11-14 10:11:38 +0100

    Ajout connexion login

--- controller/login.php
+++ controller/login.php
@@ -2,14 +2,10 @@
 session_start();
 include "./model/sqlUser.php";
 
-while ($row = mysqli_fetch_assoc($results)) {
-		
+
+while ($row = mysqli_fetch_assoc(selectAllUtilisateurs())) {
+		if $_POST['pseudo_login']==$row['pseudoUtilisateur'] and $_POST['motdepasse_login']==$row['mdpUtilisateur'] {
+		$_SESSION['connecte']=true;
+		}
 	}	
-
-
-$_POST['pseudo_login']
-$_POST['motdepasse_login']
-
-
-
 ?>
--- model/sqlUser.php
+++ model/sqlUser.php
@@ -4,3 +4,11 @@ function selectUtilisateur($id)
     $select="SELECT * FROM Utilisateurs WHERE idUtilisateur=$id";
     return mysqli_query($_SESSION['connect'], $select);
 }
+
+function selectAllUtilisateurs()
+{
+	$select="SELECT * FROM Utilisateurs ";
+	return mysqli_query($_SESSION['connect'], $select) ;
+
+}
+?>
