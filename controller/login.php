af16952b79f1d1be48a4e5c1c15a1a4a0811984c
Rémi <remibouvier99@gmail.com>
2018-11-14 10:40:42 +0100

    ajout role utilisateur dans variable session['role']

--- controller/login.php
+++ controller/login.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
 include "./model/sqlUser.php";
+include "verif.php";
 
 
 while ($row = mysqli_fetch_assoc(selectAllUtilisateurs())) {
 		if $_POST['pseudo_login']==$row['pseudoUtilisateur'] and $_POST['motdepasse_login']==$row['mdpUtilisateur'] {
 		$_SESSION['connecte']=true;
+		$_SESSION['role']=$row['roleUtilisateur'];
 		}
 	}	
 ?>
