7ed223063a002002dfadf430a8547eae89f0092a
Hamze <hamzaalrashed1@hotmail.com>
2018-11-14 09:51:36 +0100

    modif fonction afficheActus
    ajout requete pour afficher seulement le nom d'utilisateur

--- model/affichage.php
+++ model/affichage.php
@@ -1,16 +1,23 @@
 <?php
 include"sqlActus.php";
+include"sqlUser.php";
 session_start();
+
+
+
     function afficheActu(){
-        $results = mysqli_query($_SESSION['connect'], $select) or die("Erreur envoie");
-                while ($row = mysqli_fetch_assoc($results))
-                {
+        $row= mysqli_fetch_assoc(selectActus)
+        $utlisateur=mysqli_fetch_assoc(selectUtilisateur($row['utilisateurActus']));
+        while ($row)
+            {
+                   
                     echo "<section>
                             <h2>"        .$row['titreActus']."                             </h2>
-                            <h4>"        .$row['utilisateurActusdateActus']."    </h4>
+                            <h4>"        .$utlisateur['nomUtilisateur']."                    </h4>
                             <h4>"        .$row['dateActus']."                             </h4>
                             <p>"          .$row['contenuActus']."                       </p>
-                            </section>"
+                            </section>";
+                            $utlisateur=mysqli_fetch_assoc()
                 }
     }
 
